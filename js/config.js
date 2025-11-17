952381e5ca3bb494432fbcdf6886dacf5620f908
Akshaya131004 <akshayad617@gmail.com>
2025-11-18 00:10:28 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_HnOdkMQ2j', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'bt0cukspd0244b2bndc5gkj4b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_0GJiJ99q4j', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '29hbrq324j7j82jgorp1i6807q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://vpdv0dwyeh.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://za5yku49f7.execute-api.ap-south-1.amazonaws.com/wild' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
